5c97b36d5cac1d27e145a01d0a55cfbbbf07f135
Oliver Fuhrer <oli@ginko.ch>
2020-06-24 08:01:43 -0700

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 if [ ! -d serialbox ]; then
-  git clone -b master https://github.com/GridTools/serialbox.git serialbox
-  cd serialbox
-  git checkout -b savepoint_as_string
-  cd -
+  git clone --single-branch --branch savepoint_as_string https://github.com/VulcanClimateModeling/serialbox2.git serialbox
 fi
 
 docker build -t phys_standalone .
