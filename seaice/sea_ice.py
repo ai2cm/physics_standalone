6847ee9ff7e901b1a9bce2d069990b6b52df7bae
ofuhrer <oli@ginko.ch>
2020-06-29 22:41:50 +0200

    Python version

---
+++ seaice/sea_ice.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+OUT_VARS = ["tskin", "tprcp", "fice", "gflux", "ep", "stc", "tice", \
+    "snowmt", "evap", "snwdph", "chh", "weasd", "hflx", "qsurf", \
+    "hice", "cmm"]
+
+def run(in_dict):
+    # TODO - implement sea-ice model
+    return {key: in_dict.get(key, None) for key in OUT_VARS}
