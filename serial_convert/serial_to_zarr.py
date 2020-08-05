3a8a2951217a0cd499781ad5604f4cd80887bec4
Andre Perkins <frodre@gmail.com>
2020-08-05 04:28:14 +0000

    working conversion to zarr w/ dask and xarray compatibility

--- serial_convert/serial_to_zarr.py
+++ serial_convert/serial_to_zarr.py
@@ -1,8 +1,11 @@
 import zarr
 import sys
 import yaml
+import dask
+import dask.array as da
 from typing import Iterable, Mapping
 from numpy import ndarray
+from dask.diagnostics import ProgressBar
 
 SERIALBOX_DIR = "/usr/local/serialbox/"
 sys.path.append(SERIALBOX_DIR + "/python")
@@ -47,48 +50,65 @@ class SerializedPhysicsConverter():
         for var in self.var_names:
             data = init_serializer.read(var, self.savepoints[0])
 
-            # TODO: Should store constants in single element array
+            # TODO: Should store constants in single element array?
             var_info[var] = {
                 "dataset_kwargs": {
                     "shape": (self.num_savepoints, self.RANKS, *data.shape),
-                    "chunk": (1, 1, *data.shape),
-                    "dtype": data.dtype,
+                    "chunks": (1, 1, *data.shape),
+                    "dtype": str(data.dtype),
                 },
-                "is_constant": bool(data.shape)
             }
 
             if var in self.var_attrs:
-                attrs = self.var_attrs[var]
-                dims = attrs.pop("dimensions")
-                
-                # Switch to expected attr name for xarray if not a constant
-                if dims:
-                    dims = ["savepoint", "rank"] + dims
-                    attrs["_ARRAY_DIMENSIONS"] = dims
+                attrs = dict(self.var_attrs[var])
+                dims = attrs.pop("dimensions", [])
+                if not dims:
+                    dims = ["scalar"]
+                dims = ["savepoint", "rank"] + dims
+                attrs["_ARRAY_DIMENSIONS"] = dims
+                var_info[var].update(attrs)
             
         
         return var_info
 
     def _ser_to_backend(self, backend: Mapping[str, ndarray]):
-        
-        for rank, serializer in enumerate(self.serializers):
-            for var in self.var_names:
-                for k, savepoint in enumerate(self.savepoints):
 
-                    var_out = backend[var]
-                    data = serializer.read(var, savepoint)
-                    if data.size == 1:
-                        data = data.item()
-                    var_out[k, rank] = data
+        da_by_var = []
+        for var in self.var_names:
+            
+            output = backend[var]
+            tmp_dask_out = _array_to_dask_array(output, var)
+            by_savepoint = []
+            
+            for i, savepoint in enumerate(self.savepoints):
+                
+                by_rank = []
+                
+                for rank, serializer in enumerate(self.serializers):
+                    source = dask.delayed(serializer.read)(var, savepoint)
+                    dest = tmp_dask_out[i, rank]
+                    delayed_da = da.from_delayed(source, dest.shape, dtype=dest.dtype)
+                    by_rank.append(delayed_da)
+                
+                by_rank = da.stack(by_rank, axis=0)
+                by_savepoint.append(by_rank)
+
+            by_savepoint = da.stack(by_savepoint, axis=0)
+            da_by_var.append(by_savepoint)
+
+        output_arrays = [backend[var] for var in self.var_names]
+        with ProgressBar():
+            da.store(da_by_var, output_arrays)
 
     def _init_zarr(self, path: str):
 
         out = zarr.open_group(path, mode="w")
 
         for var in self.var_names:
-            info = self.var_info[var]
-            out.create_dataset(var, **info["dataset_kwargs"])
-            out.attrs
+            info = dict(self.var_info[var])
+            ds_kwargs = info.pop("dataset_kwargs", {})
+            var_group = out.create_dataset(var, **ds_kwargs)
+            var_group.attrs.update(info)
 
         return out
 
@@ -99,10 +119,23 @@ class SerializedPhysicsConverter():
 
         return out_zarr
 
+
+def _array_to_dask_array(source_array: ndarray, name: str) -> da.Array:
+
+    # Preserve chunking of zarr if it is one
+    if hasattr(source_array, "chunks"):
+        chunks = source_array.chunks
+    else:
+        chunks = None
+
+    return da.from_array(source_array, chunks=chunks, name=name)
+
+
 if __name__ == "__main__":
 
     prefix = "./turb/data"
-    metadata = yaml.safe_load("./turb/parameter_metadata.yaml")
+    with open("./serial_convert/turb_parameter_metadata.yaml", "r") as f:
+        metadata = yaml.safe_load(f)
     save = SerializedPhysicsConverter(prefix, savepoint_filter="-in-", var_attrs=metadata)
     save.save_zarr("/home/user/turb_in.zarr")
 
