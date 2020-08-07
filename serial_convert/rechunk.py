f2cb63ee31e9bdcd4c85249ed38522c645e896b7
Andre Perkins <frodre@gmail.com>
2020-08-07 19:05:05 +0000

    Switch rechunk to operate on glob for zarrs

--- serial_convert/rechunk.py
+++ serial_convert/rechunk.py
@@ -2,13 +2,14 @@ import zarr
 import rechunker
 import argparse
 import logging
-import numpy as np
+import glob
 from functools import partial
 from tempfile import TemporaryDirectory
 from pathlib import Path
 from dask.diagnostics import ProgressBar
 
 logger = logging.getLogger(__name__)
+MAX_MEM = "256MB"
 
 
 def _in_mb(nbytes_per_item, size):
@@ -38,25 +39,43 @@ def get_target_chunk(data, target_size_mb):
 
     return chunks
 
+
+def rechunk_dataset(input_path, output_path, chunksize_mb):
+
+    logger.info(f"Rechunking zarr at path: {input_path} to {output_path}")
+    dataset = zarr.open(input_path)
+    with TemporaryDirectory() as tmpdir:
+        filename = Path(input_path).name
+        tmp_zarr = Path(tmpdir, filename).as_posix()
+        chunks = {var: get_target_chunk(dataset[var], chunksize_mb) for var in dataset}
+        plan = rechunker.rechunk(dataset, chunks, MAX_MEM, output_path, tmp_zarr)
+        with ProgressBar():
+            plan.execute()
+
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_zarr")
-    parser.add_argument("output_zarr")
+    parser.add_argument(
+        "input_dir",
+        help="Directory containing zarr files to rechunk."
+    )
+    parser.add_argument(
+        "output_dir",
+        help=(
+            "Directory to place rechunked files. Note: should be different "
+            "than input to prevent file conflicts."
+        )
+    )
     parser.add_argument("--chunksize_mb", required=False, default=5, type=int)
 
     args = parser.parse_args()
 
-    max_mem = "256MB"
+    zarr_paths = glob.glob(Path(args.input_dir, "*.zarr").as_posix())
+    for path in zarr_paths:
+        in_zarr = Path(path)
+        out_zarr = Path(args.output_dir, in_zarr.name)
+        rechunk_dataset(in_zarr.as_posix(), out_zarr.as_posix(), args.chunksize_mb)
 
-    logger.info(f"Rechunking zarr at path: {args.input_zarr}")
-    dataset = zarr.open(args.input_zarr)
-    with TemporaryDirectory() as tmpdir:
-        filename = Path(args.input_zarr).name
-        tmp_zarr = Path(tmpdir, filename).as_posix()
-        chunks = {var: get_target_chunk(dataset[var], args.chunksize_mb) for var in dataset}
-        plan = rechunker.rechunk(dataset, chunks, max_mem, args.output_zarr, tmp_zarr)
-        with ProgressBar():
-            plan.execute()
+
