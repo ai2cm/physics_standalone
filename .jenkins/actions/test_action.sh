e21a30af5b91aa4853a06de002b7e2a9f96b0097
Elynn Wu <elynn510@gmail.com>
2021-06-14 11:15:22 -0700

    build venv once

--- .jenkins/actions/test_action.sh
+++ .jenkins/actions/test_action.sh
@@ -3,57 +3,48 @@ set -e -x
 scheduler_script=$1
 echo "${JOB_NAME}-${BUILD_NUMBER}"
 
-# check presence of env directory
-pushd `dirname $0` > /dev/null
-envloc=`/bin/pwd`/..
-
-# Download the env
-. ${envloc}/env.sh
-# load scheduler tools
-. ${envloc}/env/schedulerTools.sh
-
-if [ -d "venv/bin" ]
+if [ $backend == 'numpy' ] && [ $physics == 'buildenv' ] && [ -d "venv/bin" ]
 then
-    source ./venv/bin/activate
-    echo "venv already exists, continue without making one"
-else
     python -m venv venv
-    source ./venv/bin/activate
     git clone https://github.com/VulcanClimateModeling/gt4py.git
     pip install -e ./gt4py[cuda102]
     python -m gt4py.gt_src_manager install
-fi
-cd ../../
-echo `which python`
-echo `pip list`
-backend=${backend}
-phy=${physics}
-sed -i 's/<CPUSPERTASK>/12/g' ${scheduler_script}
-sed -i -e "s/<which_backend>/${backend}/g" ${scheduler_script}
-sed -i -e "s/<which_physics>/${phy}/g" ${scheduler_script}
-export IS_DOCKER=False
-echo "Submitting slurm script:"
-cat ${scheduler_script}
-
-cp ${scheduler_script} runfile/.
-cd runfile
-OUT="${phy}_${backend}.out"
-set +e
-res=$(sbatch -W -C gpu ${scheduler_script} 2>&1)
-status1=$?
-grep -q SUCCESS ${OUT}
-status2=$?
-set -e
-wait
-echo "DONE WAITING ${status1} ${status2}"
-if [ $status1 -ne 0 -o $status2 -ne 0 ] ; then
-  echo "ERROR: physics validation not successful"
-  exit 1
+    
 else
-  echo "physics validation run successful"
-fi
+    source ../../../../../numpy/physics/buildenv/slave/daint_submit/venv/bin/activate
+    echo "venv already exists, continue without making one"
+    echo $WORKSPACE
+    echo `which python`
+    echo `pip list`
+    backend=${backend}
+    phy=${physics}
+    sed -i 's/<CPUSPERTASK>/12/g' ${scheduler_script}
+    sed -i -e "s/<which_backend>/${backend}/g" ${scheduler_script}
+    sed -i -e "s/<which_physics>/${phy}/g" ${scheduler_script}
+    export IS_DOCKER=False
+    echo "Submitting slurm script:"
+    cat ${scheduler_script}
 
-echo "Job completed!"
-# echo output of SLURM job
-cat ${OUT}
-rm ${OUT}
+    cp ${scheduler_script} runfile/.
+    cd runfile
+    OUT="${phy}_${backend}.out"
+    set +e
+    res=$(sbatch -W -C gpu ${scheduler_script} 2>&1)
+    status1=$?
+    grep -q SUCCESS ${OUT}
+    status2=$?
+    set -e
+    wait
+    echo "DONE WAITING ${status1} ${status2}"
+    if [ $status1 -ne 0 -o $status2 -ne 0 ] ; then
+    echo "ERROR: physics validation not successful"
+    exit 1
+    else
+    echo "physics validation run successful"
+    fi
+
+    echo "Job completed!"
+    # echo output of SLURM job
+    cat ${OUT}
+    rm ${OUT}
+fi
