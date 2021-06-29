f758ab469971e8882247f9e743705a62979d1b01
Chris Kung <christopher.w.kung@nasa.gov>
2021-06-29 15:37:58 -0400

    Initial verified version of GT4py-ported get_phi_fv3

---
+++ get_phi_fv3/python/config.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+from gt4py import gtscript
+
+prefix = "get_prs_fv3"
+BACKEND = str(os.getenv("BACKEND")) if ("BACKEND" in os.environ) else "numpy"
+REBUILD = (os.getenv("REBUILD") == "True") if ("REBUILD" in os.environ) else True
+IS_DOCKER = (os.getenv("IS_DOCKER") == "True") if ("IS_DOCKER" in os.environ) else True
+
+DTYPE_INT = np.int32
+DTYPE_FLT = np.float64
+DTYPE_BOOL = bool
+FIELD_INT = gtscript.Field[DTYPE_INT]
+FIELD_FLT = gtscript.Field[DTYPE_FLT]
+FIELD_FLT_IJ = gtscript.Field[gtscript.IJ, DTYPE_FLT]
+FIELD_BOOL = gtscript.Field[DTYPE_BOOL]
+# Path of serialbox directory
+if IS_DOCKER:
+    SERIALBOX_DIR = "/usr/local/serialbox"
+else:
+    SERIALBOX_DIR = "/project/c14/install/daint/serialbox2_master/gnu_debug"
+
+IN_VARS = ["ix", "levs", "ntrac", "gt0", "gq0", "del_gz", "phii", "phil"]
+OUT_VARS = ["del_gz", "phii", "phil"]
+
+# Physics Constants used from physcon module
+
+con_rv = 4.6150e2
+con_rd = 2.8705e2
+con_fvirt = con_rv/con_rd - 1.0
