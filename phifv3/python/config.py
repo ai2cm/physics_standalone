492c23f888b08dd856a9eb73d6a9ec4149120b9f
Christopher Kung <68066130+gmao-ckung@users.noreply.github.com>
2021-06-30 12:05:27 -0400

    Added tests to verify `get_prs_fv3` and `get_phi_fv3` stencils (#16)
    * Create prsfv3 directory with script to get serialized data
    
    * Implemented and verified get_prs_fv3 stencil in GT4Py
    
    * Initial verified version of GT4py-ported get_phi_fv3
    
    * Added checks and verification for prsfv3 and prifv3 to runtime
    
    * Edits to have stencil backends be chosen by physics.py
    
    * Fix copy/paste error in physics.py
    
    * Removed redundant data in config.py files
    
    * Edited directory names for get_data.sh to fetch data properly.

---
+++ phifv3/python/config.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+from gt4py import gtscript
+
+prefix = "phifv3"
+BACKEND = str(os.getenv("BACKEND")) if ("BACKEND" in os.environ) else "numpy"
+REBUILD = (os.getenv("REBUILD") == "True") if ("REBUILD" in os.environ) else True
+IS_DOCKER = (os.getenv("IS_DOCKER") == "True") if ("IS_DOCKER" in os.environ) else True
+
+DTYPE_FLT = np.float64
+FIELD_FLT = gtscript.Field[DTYPE_FLT]
+# Path of serialbox directory
+if IS_DOCKER:
+    SERIALBOX_DIR = "/usr/local/serialbox"
+else:
+    SERIALBOX_DIR = "/project/c14/install/daint/serialbox2_master/gnu_debug"
+
+IN_VARS = ["ix", "levs", "ntrac", "gt0", "gq0", "del_gz", "phii", "phil"]
+OUT_VARS = ["del_gz", "phii", "phil"]
+
+# Physics Constants used from physcon module
+
+con_rv = 4.6150e2
+con_rd = 2.8705e2
+con_fvirt = con_rv/con_rd - 1.0
