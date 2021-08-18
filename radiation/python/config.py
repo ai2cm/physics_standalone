2aca4ea5785512e3e60a4de627d3233716af351a
Andrew Pauling <andrewpauling109@gmail.com>
2021-08-18 17:27:03 -0600

    cldprop now validates in gt4py

--- radiation/python/config.py
+++ radiation/python/config.py
@@ -5,7 +5,7 @@ sys.path.insert(
     0, "/Users/AndrewP/Documents/work/physics_standalone/radiation/python/radlw"
 )
 from radlw_param import nbands, maxgas, maxxsec, ngptlw
-from radsw_param import ngptsw, nbhgh, nblow
+from radsw_param import ngptsw, nbhgh, nblow, nbdsw
 from gt4py import gtscript
 from gt4py.gtscript import Field
 
@@ -37,11 +37,14 @@ shape_nlp1 = (npts, 1, nlp1)
 shape_nlp2 = (npts, 1, nlp1 + 1)
 default_origin = (0, 0, 0)
 type_nbands = (DTYPE_FLT, (nbands,))
-type_nbandssw = (DTYPE_INT, (nbhgh - nblow + 1,))
+type_nbandssw_int = (DTYPE_INT, (nbhgh - nblow + 1,))
+type_nbandssw_flt = (DTYPE_FLT, (nbhgh - nblow + 1,))
 type_ngptlw = (DTYPE_FLT, (ngptlw,))
 type_ngptsw = (DTYPE_FLT, (ngptsw,))
+type_ngptsw_bool = (DTYPE_BOOL, (ngptsw,))
 type_nbands3 = (DTYPE_FLT, (nbands, 3))
 type_maxgas = (DTYPE_FLT, (maxgas,))
 type_maxxsec = (DTYPE_FLT, (maxxsec,))
+type_nbdsw = (DTYPE_FLT, (nbdsw,))
 type_9 = (DTYPE_FLT, (9,))
 type_10 = (DTYPE_FLT, (10,))
