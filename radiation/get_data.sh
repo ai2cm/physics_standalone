628c9b37f8c3c0fda095761ecfd394a1c58b3297
Andrew Pauling <andrewpauling109@gmail.com>
2021-09-22 11:11:23 -0700

    Make get_data work outside Docker

--- radiation/get_data.sh
+++ radiation/get_data.sh
@@ -1,88 +1,96 @@
 #!/bin/bash
 
-if [ -z "$IS_DOCKER" ] ; then
+if [ ! -z "$IS_DOCKER" ] ; then
     echo "Not in docker, please build and enter the Docker image before running this script"
 else
 
-    if [ ! -d "/work/radiation/fortran/data" ]; then
-        cd /work/radiation/fortran
+    export HOME=`pwd`
+
+    if [ ! -d "./fortran/data" ]; then
+        cd ./fortran
         mkdir data
         cd data
         mkdir LW
         mkdir SW
-        cd /work/radiation
+        cd $HOME
     else
         echo "Fortran output directory already exists"
     fi
 
-    if [ -z "$(ls -A /work/radiation/fortran/data/LW)" ]; then
-        gsutil cp -r gs://vcm-fv3gfs-serialized-regression-data/physics/fv3gfs-fortran-output/radlw/* /work/radiation/fortran/data/LW/.
-        cd /work/radiation/fortran/data/LW
+    if [ -z "$(ls -A ./fortran/data/LW)" ]; then
+        gsutil cp -r gs://vcm-fv3gfs-serialized-regression-data/physics/fv3gfs-fortran-output/radlw/* ./fortran/data/LW/.
+        cd ./fortran/data/LW
         tar -xzvf data.tar.gz
+        cd $HOME
     else
         echo "LW Fortran data already present"
     fi
 
-    if [ -z "$(ls -A /work/radiation/fortran/data/SW)" ]; then
-        gsutil cp -r gs://vcm-fv3gfs-serialized-regression-data/physics/fv3gfs-fortran-output/radsw/* /work/radiation/fortran/data/SW/.
-        cd /work/radiation/fortran/data/SW
+    if [ -z "$(ls -A ./fortran/data/SW)" ]; then
+        gsutil cp -r gs://vcm-fv3gfs-serialized-regression-data/physics/fv3gfs-fortran-output/radsw/* ./fortran/data/SW/.
+        cd ./fortran/data/SW
         tar -xzvf data.tar.gz
+        cd $HOME
     else
         echo "SW Fortran data already present"
     fi
 
-    if [ -z "$(ls -A /work/radiation/python/lookupdata)" ]; then
-        gsutil cp -r gs://vcm-fv3gfs-serialized-regression-data/physics/lookupdata/lookup.tar.gz /work/radiation/python/lookupdata/.
-        cd /work/radiation/python/lookupdata
+    if [ -z "$(ls -A ./python/lookupdata)" ]; then
+        gsutil cp -r gs://vcm-fv3gfs-serialized-regression-data/physics/lookupdata/lookup.tar.gz ./python/lookupdata/.
+        cd ./python/lookupdata
         tar -xzvf lookup.tar.gz
+        cd $HOME
     else
         echo "Data already present"
     fi
 
-    if [ ! -d "/work/radiation/fortran/radlw/dump" ]; then
-        cd /work/radiation/fortran/radlw
+    if [ ! -d "./fortran/radlw/dump" ]; then
+        cd ./fortran/radlw
         mkdir dump
-        cd /work/radiation
+        cd $HOME
     else
         echo "LW standalone output directory already exists"
     fi
 
-    if [ ! -d "/work/radiation/fortran/radsw/dump" ]; then
-        cd /work/radiation/fortran/radsw
+    if [ ! -d "./fortran/radsw/dump" ]; then
+        cd ./fortran/radsw
         mkdir dump
-        cd /work/radiation
+        cd $HOME
     else
         echo "SW standalone output directory already exists"
     fi
 
-    if [ -z "$(ls -A /work/radiation/fortran/radlw/dump)" ]; then
-        gsutil cp gs://vcm-fv3gfs-serialized-regression-data/physics/standalone-output/radlw/* /work/radiation/fortran/radlw/dump/.
-        cd /work/radiation/fortran/radlw/dump
+    if [ -z "$(ls -A ./fortran/radlw/dump)" ]; then
+        gsutil cp gs://vcm-fv3gfs-serialized-regression-data/physics/standalone-output/radlw/* ./fortran/radlw/dump/.
+        cd ./fortran/radlw/dump
         tar -xzvf data.tar.gz
+        cd $HOME
     else
         echo "LW standalone data already present"
     fi
 
-    if [ -z "$(ls -A /work/radiation/fortran/radsw/dump)" ]; then
-        gsutil cp gs://vcm-fv3gfs-serialized-regression-data/physics/standalone-output/radsw/* /work/radiation/fortran/radsw/dump/.
-        cd /work/radiation/fortran/radsw/dump
+    if [ -z "$(ls -A ./fortran/radsw/dump)" ]; then
+        gsutil cp gs://vcm-fv3gfs-serialized-regression-data/physics/standalone-output/radsw/* ./fortran/radsw/dump/.
+        cd ./fortran/radsw/dump
         tar -xzvf data.tar.gz
+        cd $HOME
     else
         echo "SW standalone data already present"
     fi
 
-    if [ ! -d "/work/radiation/python/forcing" ]; then
-        cd /work/radiation/python
+    if [ ! -d "./python/forcing" ]; then
+        cd ./python
         mkdir forcing
-        cd ../
+        cd $HOME
     else
         echo "Forcing directory already exists"
     fi
 
-    if [ -z "$(ls -A /work/radiation/fortran/radlw/dump)" ]; then
-	    gsutil cp gs://vcm-fv3gfs-serialized-regression-data/physics/forcing/* /work/radiation/python/forcing/.
-	    cd /work/radiation/python/forcing
+    if [ -z "$(ls -A ./python/forcing)" ]; then
+	    gsutil cp gs://vcm-fv3gfs-serialized-regression-data/physics/forcing/* ./python/forcing/.
+	    cd ./python/forcing
 	    tar -xzvf data.tar.gz
+        cd $HOME
     else
 	    echo "Forcing data already present"
     fi  
