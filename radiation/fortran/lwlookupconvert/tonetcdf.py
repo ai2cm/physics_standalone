71389f778a839a85d6100839f2584c6d6ab42ae5
Andrew Pauling <andrewpauling109@gmail.com>
2021-09-23 16:12:49 -0700

    get lookupdata serialization code working

--- radiation/fortran/lwlookupconvert/tonetcdf.py
+++ radiation/fortran/lwlookupconvert/tonetcdf.py
@@ -1,50 +1,39 @@
 import sys
-import numpy as np
 import os
 import xarray as xr
 
-# On MacOS, remember to set the environment variable DYLD_LIBRARY_PATH to contain
-# the path to the SerialBox /lib directory
+sys.path.insert(0, "/work/radiation/python")
+from config import *
 
-os.environ["DYLD_LIBRARY_PATH"]="/Users/AndrewP/Documents/code/serialbox2/install/lib"
-
-SERIALBOX_DIR = "/Users/AndrewP/Documents/code/serialbox2/install"
-sys.path.append(SERIALBOX_DIR + "/python")
 import serialbox as ser
 
-ddir = '/Users/AndrewP/Documents/work/lookupconvert/saveData'
+ddir = "saveData"
 
-refvars = ['absa',
-           'absb',
-           'selfref',
-           'forref',
-           'fracrefa',
-           'fracrefb']
+# refvars = ["absa", "absb", "selfref", "forref", "fracrefa", "fracrefb"]
 
-dims = [('NG16', 'MSA16'),
-        ('NG16', 'MSB16'),
-        ('NG16', 'MSF16'),
-        ('NG16', 'MFR16'),
-        ('NG16', 'MAF16'),
-        'NG16']
+# dims = [
+#     ("NG16", "MSA16"),
+#     ("NG16", "MSB16"),
+#     ("NG16", "MSF16"),
+#     ("NG16", "MFR16"),
+#     ("NG16", "MAF16"),
+#     "NG16",
+# ]
 
 serializer = ser.Serializer(ser.OpenModeKind.Read, ddir, "radlw_avplnk")
-savepoints=serializer.savepoint_list()
+savepoints = serializer.savepoint_list()
 
 print(savepoints)
 
-totplnk = serializer.read('totplnk', savepoints[0])
+totplnk = serializer.read("totplnk", serializer.savepoint["mySavepoint"])
 
-dsout = xr.Dataset({'totplnk': (('nplnk', 'nbands'), totplnk)})
+dsout = xr.Dataset({"totplnk": (("nplnk", "nbands"), totplnk)})
 print(dsout)
-#dataout = dict()
-#for n, key in enumerate(refvars):
+# dataout = dict()
+# for n, key in enumerate(refvars):
 #    dataout[key] = (dims[n], serializer.read(key, savepoints[0]))
 
-#dsout = xr.Dataset(dataout)
-#print(dsout)
+# dsout = xr.Dataset(dataout)
+# print(dsout)
 
-dsout.to_netcdf('saveData/totplnk.nc')
-
-
-
+dsout.to_netcdf(os.path.join(LOOKUP_DIR, "totplnk.nc"))
--- radiation/fortran/swlookupconvert/tonetcdf.py
+++ radiation/fortran/swlookupconvert/tonetcdf.py
@@ -3,84 +3,81 @@ import numpy as np
 import os
 import xarray as xr
 
-# On MacOS, remember to set the environment variable DYLD_LIBRARY_PATH to contain
-# the path to the SerialBox /lib directory
+sys.path.insert(0, "/work/radiation/python")
+from config import *
 
-os.environ["DYLD_LIBRARY_PATH"]="/Users/AndrewP/Documents/code/serialbox2/install/lib"
-
-SERIALBOX_DIR = "/Users/AndrewP/Documents/code/serialbox2/install"
-sys.path.append(SERIALBOX_DIR + "/python")
 import serialbox as ser
 
-ddir = '/Users/AndrewP/Documents/work/swlookupconvert/saveData'
+ddir = "saveData"
 
 serializer = ser.Serializer(ser.OpenModeKind.Read, ddir, "radsw_cldprtb")
-savepoints=serializer.savepoint_list()
+savepoints = serializer.savepoint_list()
 
-refvars = ['extliq1',
-           'extliq2',
-           'ssaliq1',
-           'ssaliq2',
-           'asyliq1',
-           'asyliq2',
-           'extice2',
-           'ssaice2',
-           'asyice2',
-           'extice3',
-           'ssaice3',
-           'asyice3',
-           'fdlice3',
-           'abari',
-           'bbari',
-           'cbari',
-           'dbari',
-           'ebari',
-           'fbari',
-           'b0s',
-           'b1s',
-           'c0s',
-           'b0r',
-           'c0r']
+refvars = [
+    "extliq1",
+    "extliq2",
+    "ssaliq1",
+    "ssaliq2",
+    "asyliq1",
+    "asyliq2",
+    "extice2",
+    "ssaice2",
+    "asyice2",
+    "extice3",
+    "ssaice3",
+    "asyice3",
+    "fdlice3",
+    "abari",
+    "bbari",
+    "cbari",
+    "dbari",
+    "ebari",
+    "fbari",
+    "b0s",
+    "b1s",
+    "c0s",
+    "b0r",
+    "c0r",
+]
 
 
-dims = [('n', 'nbands'),
-        ('n', 'nbands'),
-        ('n', 'nbands'),
-        ('n', 'nbands'),
-        ('n', 'nbands'),
-        ('n', 'nbands'),
-        ('m', 'nbands'),
-        ('m', 'nbands'),
-        ('m', 'nbands'),
-        ('p', 'nbands'),
-        ('p', 'nbands'),
-        ('p', 'nbands'),
-        ('p', 'nbands'),
-        's',
-        's',
-        's',
-        's',
-        's',
-        's',
-        'nbands',
-        'nbands',
-        'nbands',
-        'nbands',
-        'nbands']
+dims = [
+    ("n", "nbands"),
+    ("n", "nbands"),
+    ("n", "nbands"),
+    ("n", "nbands"),
+    ("n", "nbands"),
+    ("n", "nbands"),
+    ("m", "nbands"),
+    ("m", "nbands"),
+    ("m", "nbands"),
+    ("p", "nbands"),
+    ("p", "nbands"),
+    ("p", "nbands"),
+    ("p", "nbands"),
+    "s",
+    "s",
+    "s",
+    "s",
+    "s",
+    "s",
+    "nbands",
+    "nbands",
+    "nbands",
+    "nbands",
+    "nbands",
+]
 
 dataout = dict()
 for n, key in enumerate(refvars):
-    dataout[key] = (dims[n], serializer.read(key, savepoints[0]))
+    dataout[key] = (dims[n], serializer.read(key, serializer.savepoint["mySavepoint"]))
 
-dataout['a0r'] = 3.07e-3
-dataout['a1r'] = 0.0
-dataout['a0s'] = 0.0
-dataout['a1s'] = 1.5
+dataout["a0r"] = 3.07e-3
+dataout["a1r"] = 0.0
+dataout["a0s"] = 0.0
+dataout["a1s"] = 1.5
 
 dsout = xr.Dataset(dataout)
 print(dsout)
 
-dsout.to_netcdf('saveData/radsw_cldprtb_data.nc')
-
-
-
+dsout.to_netcdf(os.path.join(LOOKUP_DIR, "radsw_cldprtb_data.nc"))
